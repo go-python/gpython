bb0b4cddfffeb093fbcd8a4879c15669899fd570
Nick Craig-Wood <nick@craig-wood.com>
2013-12-07 21:19:40 +0000

    Move iteration stuff into sequence.go

--- py/sequence.go
+++ py/sequence.go
@@ -33,3 +33,64 @@ func SequenceList(v Object) *List {
 		return l
 	}
 }
+
+// Call __next__ for the python object
+func Next(self Object) Object {
+	if I, ok := self.(I__next__); ok {
+		return I.M__next__()
+	} else if res, ok := TypeCall0(self, "__next__"); ok {
+		return res
+	}
+
+	panic(ExceptionNewf(TypeError, "'%s' object is not iterable", self.Type().Name))
+}
+
+// Create an iterator from obj and iterate the iterator until finished
+// calling the function passed in on each object
+func Iterate(obj Object, fn func(Object)) {
+	defer func() {
+		if r := recover(); r != nil {
+			if IsException(StopIteration, r) {
+				// StopIteration or subclass raised
+			} else {
+				panic(r)
+			}
+		}
+	}()
+	// Some easy cases
+	switch x := obj.(type) {
+	case Tuple:
+		for _, item := range x {
+			fn(item)
+		}
+	case *List:
+		for _, item := range x.Items {
+			fn(item)
+		}
+	case String:
+		for _, item := range x {
+			fn(String(item))
+		}
+	case Bytes:
+		for _, item := range x {
+			fn(Int(item))
+		}
+	default:
+		iterator := Iter(obj)
+		for {
+			item := Next(iterator)
+			fn(item)
+		}
+	}
+}
+
+// Call send for the python object
+func Send(self, value Object) Object {
+	if I, ok := self.(I_send); ok {
+		return I.Send(value)
+	} else if res, ok := TypeCall1(self, "send", value); ok {
+		return res
+	}
+
+	panic(ExceptionNewf(TypeError, "'%s' object doesn't have send method", self.Type().Name))
+}
