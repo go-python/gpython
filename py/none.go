1455b43f3e28f243af288bc726bae8f863c1aab7
Nick Craig-Wood <nick@craig-wood.com>
2014-11-19 08:29:37 +0000

    Add __str__ for None

--- py/none.go
+++ py/none.go
@@ -19,5 +19,10 @@ func (a NoneType) M__bool__() Object {
 	return False
 }
 
+func (a NoneType) M__str__() Object {
+	return String("None")
+}
+
 // Check interface is satisfied
 var _ I__bool__ = None
+var _ I__str__ = None
