5f5112abd37a96b6a56f8b591a56821d40334a99
Nick Craig-Wood <nick@craig-wood.com>
2015-03-25 21:51:15 +0000

    py.None: eq and ne

--- py/none.go
+++ py/none.go
@@ -26,3 +26,28 @@ func (a NoneType) M__str__() Object {
 // Check interface is satisfied
 var _ I__bool__ = None
 var _ I__str__ = None
+
+// Convert an Object to an NoneType
+//
+// Retrurns ok as to whether the conversion worked or not
+func convertToNoneType(other Object) (NoneType, bool) {
+	switch b := other.(type) {
+	case NoneType:
+		return b, true
+	}
+	return None, false
+}
+
+func (a NoneType) M__eq__(other Object) Object {
+	if _, ok := convertToNoneType(other); ok {
+		return True
+	}
+	return False
+}
+
+func (a NoneType) M__ne__(other Object) Object {
+	if _, ok := convertToNoneType(other); ok {
+		return False
+	}
+	return True
+}
