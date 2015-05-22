194e817c2c2bb6c33109b7ad993bec9f583e8ae5
Nick Craig-Wood <nick@craig-wood.com>
2015-05-22 17:26:37 +0100

    py: tuple - Reverse method

--- py/tuple.go
+++ py/tuple.go
@@ -27,6 +27,14 @@ func (t Tuple) Copy() Tuple {
 	copy(newT, t)
 	return newT
 }
+
+// Reverses a tuple (in-place)
+func (t Tuple) Reverse() {
+	for i, j := 0, len(t)-1; i < j; i, j = i+1, j-1 {
+		t[i], t[j] = t[j], t[i]
+	}
+}
+
 func (t Tuple) M__len__() Object {
 	return Int(len(t))
 }
