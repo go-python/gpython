0838829c002e7827acbf128b387b93e675c6d065
Nick Craig-Wood <nick@craig-wood.com>
2015-05-25 09:51:45 +0100

    vm: raise tests

---
+++ vm/tests/raise1.py
@@ -0,0 +1,6 @@
+doc="Raise not in block"
+err=ValueError("potato")
+raise err
+
+err=None
+doc="finished"
---
+++ vm/tests/raise2.py
@@ -0,0 +1,6 @@
+doc="Exception not in block"
+err=ZeroDivisionError()
+1/0
+
+err=None
+doc="finished"
