22c64260815c871e5546213c71d5ccc212220908
Kyle Ellrott <kellrott@gmail.com>
2019-06-21 12:01:37 -0700

    Adding append method to list

--- py/list.go
+++ py/list.go
@@ -13,6 +13,17 @@ type List struct {
 	Items []Object
 }
 
+func init() {
+	ListType.Dict["append"] = MustNewMethod("append", func(self Object, args Tuple) (Object, error) {
+		listSelf := self.(*List)
+		if len(args) != 1 {
+			return nil, ExceptionNewf(TypeError, "append() takes exactly one argument (%d given)", len(args))
+		}
+		listSelf.Items = append(listSelf.Items, args[0])
+		return NoneType{}, nil
+	}, 0, "append(item)")
+}
+
 // Type of this List object
 func (o *List) Type() *Type {
 	return ListType
