cf32df75566dece84ed0138755828f5248a2888c
Tim S <tim-st@users.noreply.github.com>
2019-09-15 15:30:00 +0200

    new try

--- builtin/tests/sorting.py
+++ builtin/tests/sorting.py
@@ -2,6 +2,7 @@
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
+doc = "sort: order"
 l = []
 l2 = sorted(l)
 assert l == l2
@@ -10,15 +11,21 @@ assert sorted([5, 2, 3, 1, 4]) == [1, 2, 3, 4, 5]
 a = [5, 2, 3, 1, 4]
 assert a.sort() == None
 assert a == [1, 2, 3, 4, 5]
+
+doc = "sort: dict-type as iterable"
 assert sorted({"1": "D", "2": "B", "3": "B", "5": "E", "4": "A"}) == ["1", "2", "3", "4", "5"]
 
+
+doc = "sort: complex arguments"
 kwargs = {"key": lambda l: l&1+l, "reverse": True}
 l = list(range(10))
 l.sort(**kwargs)
 assert l == sorted(range(10), **kwargs) == [8, 9, 6, 4, 5, 2, 0, 1, 3, 7]
 
+doc = "sort: different types that are comparable"
 assert sorted([1, 2, 1.1], reverse=1) == [2, 1.1, 1]
 
+doc = "sort: call syntax and types"
 try:
     sorted()
 except TypeError:
@@ -53,3 +60,5 @@ except TypeError:
     pass
 else:
     assert False
+
+doc = "finished"
