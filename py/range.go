c4de1c83ca51ed5181fa27f498a88e14f29d4c1d
HyeockJinKim <kherootz@gmail.com>
2019-09-20 22:59:35 +0900

    Add __eq__, __ne__ of range
    __eq__ compare length, start, step of range

--- py/range.go
+++ py/range.go
@@ -196,3 +196,58 @@ func computeRangeLength(start, stop, step Int) Int {
 var _ I__getitem__ = (*Range)(nil)
 var _ I__iter__ = (*Range)(nil)
 var _ I_iterator = (*RangeIterator)(nil)
+
+
+func (a *Range) M__eq__(other Object) (Object, error) {
+	b, ok := other.(*Range)
+	if !ok {
+		return NotImplemented, nil
+	}
+
+	if a.Length != b.Length {
+		return False, nil
+	}
+
+	if a.Length == 0 {
+		return True, nil
+	}
+	if a.Start != b.Start {
+		return False, nil
+	}
+
+	if a.Step == 1 {
+		return True, nil
+	}
+	if a.Step != b.Step {
+		return False, nil
+	}
+
+	return True, nil
+}
+
+func (a *Range) M__ne__(other Object) (Object, error) {
+	b, ok := other.(*Range)
+	if !ok {
+		return NotImplemented, nil
+	}
+
+	if a.Length != b.Length {
+		return True, nil
+	}
+
+	if a.Length == 0 {
+		return False, nil
+	}
+	if a.Start != b.Start {
+		return True, nil
+	}
+
+	if a.Step == 1 {
+		return False, nil
+	}
+	if a.Step != b.Step {
+		return True, nil
+	}
+
+	return False, nil
+}
