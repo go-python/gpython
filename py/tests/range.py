d886727280c81db5473294972f820c11d3c8afe3
HyeockJinKim <kherootz@gmail.com>
2019-09-21 03:36:03 +0900

    Add tests for range object

--- py/tests/range.py
+++ py/tests/range.py
@@ -38,4 +38,42 @@ assert b[-3] == 4
 assert b[-2] == 6
 assert b[-1] == 8
 
+doc="range_eq"
+assert range(10) == range(0, 10)
+assert not range(10) == 3
+assert range(20) != range(10)
+assert range(100, 200, 1) == range(100, 200)
+assert range(0, 10, 3) == range(0, 12, 3)
+assert range(2000, 100) == range(3, 1)
+assert range(0, 10, -3) == range(0, 12, -3)
+assert not range(0, 20, 2) == range(0, 20, 4)
+try:
+    range('3', 10) == range(2)
+except TypeError:
+    pass
+else:
+    assert False, "TypeError not raised"
+
+doc="range_ne"
+assert range(10, 0, -3) != range(12, 0, -3)
+assert range(10) != 3
+assert not range(100, 200, 1) != range(100, 200)
+assert range(0, 10) != range(0, 12)
+assert range(0, 10) != range(0, 10, 2)
+assert range(0, 20, 2) != range(0, 21, 2)
+assert range(0, 20, 2) != range(0, 20, 4)
+assert not range(0, 20, 3) != range(0, 20, 3)
+try:
+    range('3', 10) != range(2)
+except TypeError:
+    pass
+else:
+    assert False, "TypeError not raised"
+
+doc="range_str"
+assert str(range(10)) == 'range(0, 10)'
+assert str(range(10, 0, 3)) == 'range(10, 0, 3)'
+assert str(range(0, 3)) == 'range(0, 3)'
+assert str(range(10, 3, -2)) == 'range(10, 3, -2)'
+
 doc="finished"
