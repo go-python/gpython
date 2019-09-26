2da2efab0c894f8f9803e6c6275bf8d33c719cc2
HyeockJinKim <kherootz@gmail.com>
2019-09-26 23:54:23 +0900

    Add tests for range

--- py/tests/range.py
+++ py/tests/range.py
@@ -76,4 +76,44 @@ assert str(range(10, 0, 3)) == 'range(10, 0, 3)'
 assert str(range(0, 3)) == 'range(0, 3)'
 assert str(range(10, 3, -2)) == 'range(10, 3, -2)'
 
+doc="range_slice"
+a = range(10)
+assert a[::-1][0] == 9
+assert a[::-1][9] == 0
+assert a[0:3][0] == 0
+assert a[0:3][2] == 2
+assert a[-3:10][0] == 7
+assert a[-100:13][0] == 0
+assert a[-100:13][9] == 9
+
+try:
+    a[0:3][3]
+except IndexError:
+    pass
+else:
+    assert False, "IndexError not raised"
+try:
+    a[100:13][0]
+except IndexError:
+    pass
+else:
+    assert False, "IndexError not raised"
+try:
+    a[0:3:0]
+except ValueError:
+    pass
+else:
+    assert False, "ValueError not raised"
+
+doc="range_index"
+class Index:
+    def __index__(self):
+        return 1
+
+a = range(10)
+b = Index()
+assert a[b] == 1
+assert a[b:10] == a[1:10]
+assert a[10:b:-1] == a[10:1:-1]
+
 doc="finished"
