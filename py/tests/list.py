f411862f3a8545ecb0a9b9e75828d367e0a8c680
Tim S <tim-st@users.noreply.github.com>
2019-09-28 11:27:49 +0200

    Add tests for `list.sort`

--- py/tests/list.py
+++ py/tests/list.py
@@ -1,4 +1,4 @@
-# Copyright 2018 The go-python Authors.  All rights reserved.
+# Copyright 2019 The go-python Authors.  All rights reserved.
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
@@ -38,5 +38,38 @@ a = [1, 2, 3]
 assert a * 2  == [1, 2, 3, 1, 2, 3]
 assert a * 0 == []
 assert a * -1 == []
+
+doc="sort"
+a = [3, 1.1, 1, 2]
+s1 = list(a)
+s1.sort()
+assert s1 == [1, 1.1, 2, 3]
+s1.sort() # sort a sorted list
+assert s1 == [1, 1.1, 2, 3]
+s2 = list(a)
+s2.sort(reverse=True)
+assert s2 == [3, 2, 1.1, 1]
+s2.sort() # sort a reversed list
+assert s2 == [1, 1.1, 2, 3]
+s3 = list(a)
+s3.sort(key=lambda l: l+1) # test lambda key
+assert s3 == [1, 1.1, 2, 3]
+s4 = [2.0, 2, 1, 1.0]
+s4.sort(key=lambda l: 0) # test stability
+assert s4 == [2.0, 2, 1, 1.0]
+assert [type(t) for t in s4] == [float, int, int, float]
+s4 = [2.0, 2, 1, 1.0]
+s4.sort() # test stability
+assert s4 == [1, 1.0, 2.0, 2]
+assert [type(t) for t in s4] == [int, float, float, int]
+s5 = [2.0, "abc"]
+assertRaises(TypeError, lambda: s5.sort())
+s5 = []
+s5.sort()
+assert s5 == []
+s5 = [0]
+s5.sort()
+assert s5 == [0]
+assertRaises(TypeError, lambda: s5.sort(key=1))
 
 doc="finished"
