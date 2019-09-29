95ae69d53bcdffb27a0265a8060aaac475506324
Tim S <tim-st@users.noreply.github.com>
2019-09-29 21:52:43 +0200

    Update float.py

--- py/tests/float.py
+++ py/tests/float.py
@@ -12,6 +12,9 @@ assert float("1.5 ") == 1.5
 assert float("-1E9") == -1E9
 assert float("1E400") == float("inf")
 assert float(" -1E400") == float("-inf")
+assertRaises(ValueError, float, "1 E200")
+
+doc="repr"
 assert repr(float("1.0")) == "1.0"
 assert repr(float("1.")) == "1.0"
 assert repr(float("1.1")) == "1.1"
@@ -20,6 +23,15 @@ assert repr(float("-1.0")) == "-1.0"
 assert repr(float("1.00101")) == "1.00101"
 assert repr(float("1.00")) == "1.0"
 assert repr(float("2.010")) == "2.01"
-assertRaises(ValueError, float, "1 E200")
+
+doc="str"
+assert str(float("1.0")) == "1.0"
+assert str(float("1.")) == "1.0"
+assert str(float("1.1")) == "1.1"
+assert str(float("1.11")) == "1.11"
+assert str(float("-1.0")) == "-1.0"
+assert str(float("1.00101")) == "1.00101"
+assert str(float("1.00")) == "1.0"
+assert str(float("2.010")) == "2.01"
 
 doc="finished"
