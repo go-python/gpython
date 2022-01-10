6a0c11edd35783d8dbda100cc33bebc88dae1226
Drew O'Meara <drew@plan-systems.org>
2022-01-10 10:41:30 -0600

    license and formatting

--- examples/multi-ctx/main.go
+++ examples/multi-ctx/main.go
@@ -1,3 +1,7 @@
+// Copyright 2022 The go-python Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package main
 
 import (
@@ -15,21 +19,20 @@ import (
 func main() {
 
 	// The total job count implies a fixed amount of work.
-    // The number of workers is how many py.Ctx (in concurrent goroutines) to pull jobs off the queue.
-    // One worker does all the work serially while N number of workers will (ideally) divide that up. 
-    totalJobs := 20
-    
-    for i := 0; i < 10; i++ {
-        numWorkers := i + 1
-        elapsed := RunMultiPi(numWorkers, totalJobs)
-        fmt.Printf("=====> %2d worker(s): %v\n\n", numWorkers, elapsed)
-        
-        // Give each trial a fresh start
-        runtime.GC()
-    }
-    
-}
+	// The number of workers is how many py.Ctx (in concurrent goroutines) to pull jobs off the queue.
+	// One worker does all the work serially while N number of workers will (ideally) divide that up.
+	totalJobs := 20
 
+	for i := 0; i < 10; i++ {
+		numWorkers := i + 1
+		elapsed := RunMultiPi(numWorkers, totalJobs)
+		fmt.Printf("=====> %2d worker(s): %v\n\n", numWorkers, elapsed)
+
+		// Give each trial a fresh start
+		runtime.GC()
+	}
+
+}
 
 var jobScript = `
 pi = chud.pi_chudnovsky_bs(numDigits)
@@ -54,16 +57,16 @@ type worker struct {
 
 func (w *worker) compileTemplate(pySrc string) {
 	pySrc = strings.Replace(pySrc, "{{WORKER_ID}}", w.name, -1)
-    	
-    mainImpl := py.ModuleImpl{
-        CodeSrc: pySrc,
-    }
-    
-    var err error
-    w.main, err = w.ctx.ModuleInit(&mainImpl)
-    if err != nil {
-        log.Fatal(err)
-    }
+
+	mainImpl := py.ModuleImpl{
+		CodeSrc: pySrc,
+	}
+
+	var err error
+	w.main, err = w.ctx.ModuleInit(&mainImpl)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func RunMultiPi(numWorkers, numTimes int) time.Duration {
@@ -74,60 +77,57 @@ func RunMultiPi(numWorkers, numTimes int) time.Duration {
 	jobPipe := make(chan int)
 	go func() {
 		for i := 0; i < numTimes; i++ {
-			jobPipe <- i+1
+			jobPipe <- i + 1
 		}
 		close(jobPipe)
 	}()
-	
+
 	// Note that py.Code can be shared (accessed concurrently) since it is an inherently read-only object
 	jobCode, err := py.Compile(jobScript, "<jobScript>", py.ExecMode, 0, true)
 	if err != nil {
-	    log.Fatal("jobScript failed to comple")
+		log.Fatal("jobScript failed to comple")
 	}
 
 	workers := make([]worker, numWorkers)
 	for i := 0; i < numWorkers; i++ {
 
-        opts := py.DefaultCtxOpts()
-        
-        // Make sure our import statement will find pi_chudnovsky_bs
-        opts.SysPaths = append(opts.SysPaths, "..")
+		opts := py.DefaultCtxOpts()
+
+		// Make sure our import statement will find pi_chudnovsky_bs
+		opts.SysPaths = append(opts.SysPaths, "..")
 
 		workers[i] = worker{
 			name: fmt.Sprintf("Worker #%d", i+1),
 			ctx:  py.NewCtx(opts),
-            job:  jobCode,
+			job:  jobCode,
 		}
-		
+
 		workersRunning.Add(1)
-    }
-    
-    startTime := time.Now()
-    
+	}
+
+	startTime := time.Now()
+
 	for i := range workers {
-	    w := workers[i]
+		w := workers[i]
 		go func() {
-		
-		    // Compiling can be concurrent since there is no associated py.Ctx
-            w.compileTemplate(jobSrcTemplate)
-            
+
+			// Compiling can be concurrent since there is no associated py.Ctx
+			w.compileTemplate(jobSrcTemplate)
+
 			for jobID := range jobPipe {
-			    numDigits := 100000
-			    if jobID % 2 == 0 {
-			        numDigits *= 10
-                }
-			    py.SetAttrString(w.main.Globals, "numDigits", py.Int(numDigits))
-			    py.SetAttrString(w.main.Globals, "jobID", py.Int(jobID))
-                w.ctx.RunCode(jobCode, w.main.Globals, w.main.Globals, nil)
+				numDigits := 100000
+				if jobID%2 == 0 {
+					numDigits *= 10
+				}
+				py.SetAttrString(w.main.Globals, "numDigits", py.Int(numDigits))
+				py.SetAttrString(w.main.Globals, "jobID", py.Int(jobID))
+				w.ctx.RunCode(jobCode, w.main.Globals, w.main.Globals, nil)
 			}
 			workersRunning.Done()
 		}()
 	}
 
 	workersRunning.Wait()
-	
+
 	return time.Since(startTime)
 }
-
-
-
