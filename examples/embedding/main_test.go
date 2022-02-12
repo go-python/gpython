4a49a6d8eed18fa591fd30abe05c0f8046ca4c32
Drew O'Meara <drew@plan-systems.org>
2022-02-12 10:08:48 -0600

    CI-friendly golden testing fixes

--- examples/embedding/main_test.go
+++ examples/embedding/main_test.go
@@ -2,27 +2,33 @@ package main
 
 import (
 	"bytes"
+	"flag"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"testing"
 )
 
-const embeddingTestOutput = "testdata/embedding_golden.txt"
+const embeddingTestOutput = "testdata/embedding_out_golden.txt"
+
 var regen = flag.Bool("regen", false, "regenerate golden files")
 
 func TestEmbeddedExample(t *testing.T) {
 
 	tmp, err := os.MkdirTemp("", "go-python-embedding-")
-	if err != nil { t.Fatal(err) }
+	if err != nil {
+		t.Fatal(err)
+	}
 	defer os.RemoveAll(tmp)
-	cmd := exec.Command("go", "build", "-o", filepath.Join(tmp,"exe"), ".")
-	err := cmd.Run()
+
+	cmd := exec.Command("go", "build", "-o", filepath.Join(tmp, "exe"), ".")
+	err = cmd.Run()
 	if err != nil {
 		t.Fatalf("failed to compile embedding example: %v", err)
 	}
 
 	out := new(bytes.Buffer)
-	cmd = exec.Command(filepath.Join(tmp,"exe"), "mylib-demo.py")
+	cmd = exec.Command(filepath.Join(tmp, "exe"), "mylib-demo.py")
 	cmd.Stdout = out
 
 	err = cmd.Run()
@@ -31,17 +37,20 @@ func TestEmbeddedExample(t *testing.T) {
 	}
 
 	testOutput := out.Bytes()
+
+	flag.Parse()
 	if *regen {
 		err = os.WriteFile(embeddingTestOutput, testOutput, 0644)
 		if err != nil {
 			t.Fatalf("failed to write test output: %v", err)
 		}
 	}
-		mustMatch, err := os.ReadFile(embeddingTestOutput)
-		if err != nil {
-			t.Fatalf("failed read %q", embeddingTestOutput)
-		}
-		if !bytes.Equal(testOutput, mustMatch) {
-			t.Fatalf("embedded test output did not match accepted output from %q", embeddingTestOutput)
-		}
+
+	mustMatch, err := os.ReadFile(embeddingTestOutput)
+	if err != nil {
+		t.Fatalf("failed read %q", embeddingTestOutput)
+	}
+	if !bytes.Equal(testOutput, mustMatch) {
+		t.Fatalf("embedded test output did not match accepted output from %q", embeddingTestOutput)
+	}
 }
